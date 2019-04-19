589b802c68940bb1d09807251c9fbd15682fc79f
irfan87 <nerve2009@yahoo.com>
2019-04-19 12:02:49 +0800

    handle the error if page / routes not found

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
 
+// handle the available routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// handling the error if page not found
+app.use((req, res, next) => {
+    const error = new Error('Page Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
