7029236bf27b5c1a399312963151a06ab716427f
irfan87 <nerve2009@yahoo.com>
2019-04-19 11:55:33 +0800

    add morgan packages

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
 // import products query from products.js
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
